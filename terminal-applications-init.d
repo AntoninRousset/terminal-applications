9436e3c770bd8e84c6a401cbb2dc423e2af5fdc0
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-01-30 16:33:44 +0100

    temporary bind

--- terminal-applications-init.d
+++ terminal-applications-init.d
@@ -6,16 +6,28 @@ description="Workaround of the bug that prevents desktop files to launch a termi
 
 : ${SRC_FOLDERS:="/usr/share/applications"}
 : ${TERMCMD:="xterm -e"}
+: ${TMP_FOLDER:="/tmp/.applications"}
 
 depend() {
 	need localmount
 }
 
 start() {
-	ebegin "Changing destkop files $SRC_FOLDERS"
+	mkdir -p "$TMP_FOLDER"
 
 	for src_folder in $SRC_FOLDERS ; do
-		for desktop_file in ${src_folder}/* ; do
+
+		ebegin "Binding applications folder: $src_folder"
+		mkdir -p "$TMP_FOLDER/$src_folder"
+		cp -fr "$src_folder/"* "$TMP_FOLDER$src_folder"/
+		mount -o bind "$TMP_FOLDER/$src_folder" "$src_folder"
+
+	done
+
+	ebegin "Changing destkop files"
+	for src_folder in $SRC_FOLDERS ; do
+		
+		for desktop_file in "$TMP_FOLDER/$src_folder/"* ; do
 			if grep -q "Terminal=true" "$desktop_file" ; then
 				sed -i "s/^Terminal=true/Terminal=false/;/Exec=$TERMCMD /!s/^Exec=/Exec=$TERMCMD /" "$desktop_file"
 			fi
@@ -26,16 +38,14 @@ start() {
 }
 
 stop() {
-	ebegin "Reverting destkop files $SRC_FOLDERS"
-	
-	for src_folder in $SRC_FOLDERS ; do
-		for desktop_file in ${src_folder}/* ; do
-			if grep -q "Exec=$TERMCMD " "$desktop_file" ; then
-				sed -i "s/^Terminal=false/Terminal=true/;s/^Exec=$TERMCMD /Exec=/" "$desktop_file"
-			fi
-		done
-	done
+	ebegin "Reverting applications folders"
 
+	for src_folder in $SRC_FOLDERS ; do
+
+		umount -q $src_folder
+
+	done
+	rm -fr $TMP_FOLDER
 
 	eend 0
 }
