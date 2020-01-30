f2b6d13e383fdfa543235c56398921a667dd91d6
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-01-30 15:11:36 +0100

    should work now

--- terminal-applications-init.d
+++ terminal-applications-init.d
@@ -7,8 +7,6 @@ description="Workaround of the bug that prevents desktop files to launch a termi
 : ${SRC_FOLDERS:="/usr/share/applications"}
 : ${TERMCMD:="xterm -e"}
 
-modified_files=""
-
 depend() {
 	after procfs
 }
@@ -19,8 +17,7 @@ start() {
 	for src_folder in $SRC_FOLDERS ; do
 		for desktop_file in ${src_folder}/* ; do
 			if grep -q "Terminal=true" "$desktop_file" ; then
-# sed -i "s/^Terminal=true/Terminal=false/;s/^Exec=/Exec=$TERMCMD /" $1
-				modified_files="$modified_files $desktop_file"
+				sed -i "s/^Terminal=true/Terminal=false/;/Exec=$TERMCMD /!s/^Exec=/Exec=$TERMCMD /" "$desktop_file"
 			fi
 		done
 	done
@@ -31,13 +28,11 @@ start() {
 stop() {
 	ebegin "Removing desktop files from $DEST_FOLDER"
 	
-	echo $modified_files
-
 	for src_folder in $SRC_FOLDERS ; do
 		for desktop_file in ${src_folder}/* ; do
-			if grep -q "Exec=$TERMCMD" "$desktop_file" ; then
-#sed -i "/^Terminal=/d;s/^Exec=$TERMCMD /Exec=/" $1
-				echo ""
+			if grep -q "Exec=$TERMCMD " "$desktop_file" ; then
+				echo $desktop_file
+				sed -i "s/^Terminal=false/Terminal=true/;s/^Exec=$TERMCMD /Exec=/" "$desktop_file"
 			fi
 		done
 	done
