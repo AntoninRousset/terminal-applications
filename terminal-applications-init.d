0295f4f0e59ff3604109f487d5fde7244905c554
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-01-30 13:54:38 +0100

    fixes

--- terminal-applications-init.d
+++ terminal-applications-init.d
@@ -4,8 +4,7 @@
 
 description="Workaround of the bug that prevents desktop files to launch a terminal with Terminal=true"
 
-: ${SRC_FOLDERS:="/usr/share/applications/"}
-: ${DEST_FOLDER:="/home/antonin/.local/share/applications/"}
+: ${SRC_FOLDERS:="/usr/share/applications"}
 : ${TERMCMD:="xterm -e"}
 
 depend() {
@@ -17,7 +16,7 @@ start() {
 
 	create_file() {
 		name=$(basename $1)
-		sed "/^Terminal=/d;s/^Exec=/Exec=$TERMCMD /" $1 > ${DEST_FOLDER}$name
+		sed "/^Terminal=/d;s/^Exec=/Exec=$TERMCMD /" $1 > $1
 	}
 
 	for src_folder in $SRC_FOLDERS ; do
@@ -34,11 +33,12 @@ start() {
 stop() {
 	ebegin "Removing desktop files from $DEST_FOLDER"
 	
-	for desktop_file in ${DEST_FOLDER}/* ; do
-		if grep -q "Exec=$TERMCMD" "$desktop_file" ; then
-			echo $desktop_file
-			rm $desktop_file
-		fi
+	for src_folder in $SRC_FOLDERS ; do
+		for desktop_file in ${src_folder}/* ; do
+			if grep -q "Exec=$TERMCMD" "$desktop_file" ; then
+				sed "/^Terminal=/d;s/^Exec=$TERMCMD /Exec=/" $1 > $1
+			fi
+		done
 	done
 
 
