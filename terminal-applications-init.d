06f22fa39ecacd61fc9e364c4fa38778721f5bcf
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-01-31 08:24:00 +0100

    cleanup

--- terminal-applications-init.d
+++ terminal-applications-init.d
@@ -7,19 +7,19 @@ description="Workaround of the bug that prevents desktop files to launch a termi
 : ${SRC_FOLDERS:="/usr/share/applications"}
 : ${TERMCMD:="xterm -e"}
 
-TERMINALS="$TERMINAL|[aEkx]term|rxvt|gnome|konsole|interix|st|xterm"
-TERMINAL_PARTS="$TERMINALS|-\w*|\s"
+TERMINALS="${TERMINALS}|[aEkx]term|rxvt|gnome|konsole|interix|st|xterm"
+TERMINAL_PARTS="${TERMINALS}|-\w*|\s"
 
 depend() {
 	need localmount
 }
 
 start() {
-	for src_folder in $SRC_FOLDERS ; do
-		ebegin "Changing destkop files in $src_folder"
+	for src_folder in ${SRC_FOLDERS} ; do
+		ebegin "Changing destkop files in ${src_folder}"
 		for desktop_file in "${src_folder}/"* ; do
-			if grep -Eq "Terminal\s*=\s*true" "$desktop_file" ; then
-				sed -ri "s/^Terminal\s*=\s*true/Terminal=false/;s/^Exec\s*=($TERMINAL_PARTS)*/Exec=$TERMCMD /" "$desktop_file"
+			if grep -Eq "Terminal\s*=\s*true" "${desktop_file}" ; then
+				sed -ri "s/^Terminal\s*=\s*true/Terminal=false/;s/^Exec\s*=(${TERMINAL_PARTS})*/Exec=${TERMCMD} /" "${desktop_file}"
 			fi
 		done
 	done
@@ -29,10 +29,10 @@ start() {
 
 stop() {
 	for src_folder in $SRC_FOLDERS ; do
-		ebegin "Reverting destkop files $src_folder"
+		ebegin "Reverting destkop files ${src_folder}"
 		for desktop_file in "${src_folder}/"* ; do
-			if grep -Eq "Exec\s*=\s*($TERMINALS)" "$desktop_file" ; then
-				sed -ri "s/^Terminal\s*=\s*false/Terminal=true/;s/^Exec\s*=($TERMINAL_PARTS)*/Exec=/" "$desktop_file"
+			if grep -Eq "Exec\s*=\s*(${TERMINALS})" "${desktop_file}" ; then
+				sed -ri "s/^Terminal\s*=\s*false/Terminal=true/;s/^Exec\s*=(${TERMINAL_PARTS})*/Exec=/" "${desktop_file}"
 			fi
 		done
 	done
