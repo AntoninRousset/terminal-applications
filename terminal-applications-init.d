1afba59c9abffd3c6f07fa49a79ca129a6f1eb19
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-01-31 10:25:43 +0100

     smarter

--- terminal-applications-init.d
+++ terminal-applications-init.d
@@ -4,11 +4,14 @@
 
 description="Workaround of the bug that prevents desktop files to launch a terminal with Terminal=true"
 
-: ${SRC_FOLDERS:="/usr/share/applications"}
+#: ${SRC_FOLDERS:="/usr/share/applications"}
+: ${SRC_FOLDERS:="/tmp/applications"}
 : ${TERMCMD:="xterm -e"}
 
-TERMINALS="${TERMINALS}|[aEkx]term|rxvt|gnome|konsole|interix|st|xterm"
-TERMINAL_PARTS="${TERMINALS}|-\w*|\s"
+TERMCMDS="[aEkx]term -e|rxvt -e|gnome -e|konsole -e|interix -e|st -e|xterm -e"
+
+terminal_cmds=$(echo "${TERMCMDS}" | sed -r "s/\|/(\\\s|-\w*)*\|/g;s/\s/(\\\s|-\w*)*/g")
+terminal_parts=$(echo "${TERMCMDS}" | sed -r "s/(\s|-\w*)//g")"|-\w*|\s"
 
 depend() {
 	need localmount
@@ -19,7 +22,7 @@ start() {
 		ebegin "Changing destkop files in ${src_folder}"
 		for desktop_file in "${src_folder}/"* ; do
 			if grep -Eq "Terminal\s*=\s*true" "${desktop_file}" ; then
-				sed -ri "s/^Terminal\s*=\s*true/Terminal=false/;s/^Exec\s*=(${TERMINAL_PARTS})*/Exec=${TERMCMD} /" "${desktop_file}"
+				sed -ri "s/^Terminal\s*=\s*true/Terminal=false/;s/^Exec\s*=(${terminal_parts})*/Exec=${TERMCMD} /" "${desktop_file}"
 			fi
 		done
 	done
@@ -31,8 +34,8 @@ stop() {
 	for src_folder in $SRC_FOLDERS ; do
 		ebegin "Reverting destkop files ${src_folder}"
 		for desktop_file in "${src_folder}/"* ; do
-			if grep -Eq "Exec\s*=\s*(${TERMINALS})" "${desktop_file}" ; then
-				sed -ri "s/^Terminal\s*=\s*false/Terminal=true/;s/^Exec\s*=(${TERMINAL_PARTS})*/Exec=/" "${desktop_file}"
+			if grep -Eq "Exec\s*=\s*(${terminal_cmds})" "${desktop_file}" ; then
+				sed -ri "s/^Terminal\s*=\s*false/Terminal=true/;s/^Exec\s*=(${terminal_parts})*/Exec=/" "${desktop_file}"
 			fi
 		done
 	done
